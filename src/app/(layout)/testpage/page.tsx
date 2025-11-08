0cd9d03a58e36c22961d58a2d3dd4979ee3dcba6
sunny-son <thstkd92@gmail.com>
2025-11-08 13:44:59 +0900

    feat : 로고 컴포넌트 추가

--- src/app/(layout)/testpage/page.tsx
+++ src/app/(layout)/testpage/page.tsx
@@ -1,7 +1,10 @@
+import FinsightIcon from '@/assets/icons/finsight-logo.svg';
+
 //todo: 테스트 페이지로 삭제해야함
 export default function page() {
   return (
     <div>
+      <FinsightIcon />
       <h1 className='text-20 font-medium text-red500'>
         Title1
       </h1>
