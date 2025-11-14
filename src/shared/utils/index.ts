ebfb288de5bfd196c50764b051e1d4f69dcce7d7
mmeat512 <58766047+mmeat512@users.noreply.github.com>
2025-11-14 21:10:48 +0900

    feat: 날짜 변환 함수 추가

---
+++ src/shared/utils/index.ts
@@ -0,0 +1,9 @@
+// "2025-11-14T20:21:03" -> 2025.10.27
+export function formatDateKorean(dateString: string): string {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1; // 월은 0부터 시작하므로 1을 더해줍니다.
+  const day = date.getDate();
+
+  return `${year}.${month}.${day}`;
+}
