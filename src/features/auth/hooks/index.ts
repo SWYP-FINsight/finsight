1ee44e4b4affecb1cd87bf10dcf73977593aa055
sunny-son <thstkd92@gmail.com>
2025-11-14 22:49:39 +0900

    feat : 로그인 API fetch 추가

--- src/features/auth/hooks/index.ts
+++ src/features/auth/hooks/index.ts
@@ -1,13 +1,28 @@
-import { getAuthMe } from '@/features/auth/api';
-import { AuthMeResponse } from '@/features/auth/types';
+import { getAuthMe, loginUser } from '@/features/auth/api';
+import { AuthMeResponse, LoginRequest } from '@/features/auth/types';
+import { ApiResponse } from '@/features/common/types';
 import { HttpError } from '@/lib/apiClient';
 import { QUERY_STALE_TIME } from '@/shared/constants';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 export const useAuthMe = () => {
   return useQuery<AuthMeResponse, HttpError>({
     queryKey: ['auth', 'me'],
     queryFn: getAuthMe,
     staleTime: QUERY_STALE_TIME,
+  });
+};
+
+export const useLoginMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation<ApiResponse, HttpError, LoginRequest>({
+    mutationFn: loginUser,
+    onSuccess: (data) => {
+      console.log(data.message);
+      queryClient.invalidateQueries({ queryKey: ['auth', 'me'] });
+    },
+    onError: (err) => {
+      console.error('로그인 실패');
+    },
   });
 };
--- src/features/auth/types.ts
+++ src/features/auth/types.ts
@@ -1,5 +1,10 @@
 import { ApiResponse } from '@/features/common/types';
 
+export interface LoginRequest {
+  username: string;
+  password: string;
+}
+
 export interface AuthMeResponse extends ApiResponse {
   data: {
     username: string;
