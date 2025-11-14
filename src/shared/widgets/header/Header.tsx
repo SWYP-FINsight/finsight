296d54fefc923273af13109aa2c6f50eba09e15b
sunny-son <thstkd92@gmail.com>
2025-11-14 15:25:51 +0900

    feat : 로고 클릭시 홈 화면 이동 기능 추가

--- src/shared/widgets/header/Header.tsx
+++ src/shared/widgets/header/Header.tsx
@@ -4,6 +4,7 @@ import LogoIcon from '@/assets/icons/finsight-logo.svg';
 import LoggedIn from '@/features/auth/components/LoggedIn';
 import { useAuthMe } from '@/features/auth/hooks';
 import DefaultButton from '@/shared/ui/button/DefaultButton';
+import Link from 'next/link';
 
 export default function Header() {
   const { data: response, isLoading } = useAuthMe();
@@ -14,7 +15,9 @@ export default function Header() {
 
   return (
     <header className="flex w-full p-[1.6rem] justify-between items-center">
-      <LogoIcon />
+      <Link href="/">
+        <LogoIcon />
+      </Link>
       <div>
         {isLoading ? (
           <h3>로딩중</h3>
