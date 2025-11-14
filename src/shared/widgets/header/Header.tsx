84ef929060188bc553a3e0f7b69be3a66d75d40c
sunny-son <thstkd92@gmail.com>
2025-11-14 15:24:47 +0900

    feat : 헤더 컴포넌트 추가

---
+++ src/shared/widgets/header/Header.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import LogoIcon from '@/assets/icons/finsight-logo.svg';
+import LoggedIn from '@/features/auth/components/LoggedIn';
+import { useAuthMe } from '@/features/auth/hooks';
+import DefaultButton from '@/shared/ui/button/DefaultButton';
+
+export default function Header() {
+  const { data: response, isLoading } = useAuthMe();
+  console.log(response);
+
+  const isLoggedIn = response?.data?.loggedIn === true;
+  const username = response?.data?.username;
+
+  return (
+    <header className="flex w-full p-[1.6rem] justify-between items-center">
+      <LogoIcon />
+      <div>
+        {isLoading ? (
+          <h3>로딩중</h3>
+        ) : isLoggedIn ? (
+          <LoggedIn username={username} />
+        ) : (
+          <DefaultButton text="로그인" color="brand500" textColor="gray-50" />
+        )}
+      </div>
+    </header>
+  );
+}
