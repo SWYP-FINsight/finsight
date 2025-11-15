688c919caa7f0e3e83732a77bcd3f753061019b5
sunny-son <thstkd92@gmail.com>
2025-11-15 23:16:02 +0900

    feat : 회원가입 성공시 성공 모달 띄우도록 변경

--- src/shared/ui/modal/AlertModal.tsx
+++ src/shared/ui/modal/AlertModal.tsx
@@ -4,29 +4,36 @@ import ErrorIcon from '@/assets/icons/error-circle.svg';
 import { HTMLAttributes, ReactNode } from 'react';
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
-  isOpen: boolean;
-  onClose: () => void;
+  isOpen?: boolean;
+  onClose?: () => void;
   children: ReactNode;
   modalType?: 'success' | 'error';
 }
 
 export default function AlertModal({ isOpen, onClose, children, modalType, ...rest }: Props) {
+  if (!isOpen) {
+    return null;
+  }
+
   const Icon = modalType === 'success' ? SuccessIcon : ErrorIcon;
+
   return (
-    <div
-      className="relative flex flex-col items-center gap-[1.2rem] w-[28.5rem] h-[13.1rem] bg-gray-100
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-4" onClick={onClose}>
+      <div
+        className="relative flex flex-col items-center gap-[1.2rem] w-[28.5rem] h-[13.1rem] bg-gray-100
         py-[1.8rem] px-[1.6rem] rounded-[2rem]"
-      {...rest}
-    >
-      <button
-        type="button"
-        onClick={onClose}
-        className="absolute flex justify-center items-center w-[2.4rem] h-[2.4rem] top-[1.8rem] right-[1.6rem]"
+        {...rest}
       >
-        <CloseIcon width="1rem" height="1rem" />
-      </button>
-      {Icon && <Icon className="w-[5rem] h-[5rem]" />}
-      <div className="text-gray-900 text-[1.8rem] font-bold text-center">{children}</div>
+        <button
+          type="button"
+          onClick={onClose}
+          className="absolute flex justify-center items-center w-[2.4rem] h-[2.4rem] top-[1.8rem] right-[1.6rem] cursor-pointer"
+        >
+          <CloseIcon width="1rem" height="1rem" />
+        </button>
+        {Icon && <Icon className="w-[5rem] h-[5rem]" />}
+        <div className="text-gray-900 text-[1.8rem] font-bold text-center">{children}</div>
+      </div>
     </div>
   );
 }
