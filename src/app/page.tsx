c015037d5b8f5f40f2c45358563417151512763a
mmeat512 <58766047+mmeat512@users.noreply.github.com>
2025-11-15 20:55:52 +0900

    fix: build 에러 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,17 @@ import ArticleList from '@/features/articles/components/ArticleList';
 import DateFilter from '@/features/articles/components/DateFilter';
 import SearchComponent from '@/features/articles/components/SearchComponent';
 import Header from '@/shared/widgets/header/Header';
+import { Suspense } from 'react';
 
 export default function Home() {
   return (
     <div className="flex flex-col">
-      <Header />
-      <SearchComponent />
-      <DateFilter />
-      <ArticleList />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Header />
+        <SearchComponent />
+        <DateFilter />
+        <ArticleList />
+      </Suspense>
     </div>
   );
 }
