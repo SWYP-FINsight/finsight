5ce382d4cd4b5b508f1b2fa6e7508d3d2e28fdf2
sunny-son <thstkd92@gmail.com>
2025-11-16 17:38:08 +0900

    refactor : 로딩 컴포넌트 리팩터링

--- src/features/my-collection/components/CollectionList.tsx
+++ src/features/my-collection/components/CollectionList.tsx
@@ -1,23 +1,16 @@
 'use client';
+
 import AddFolderIcon from '@/assets/icons/add-folder.svg';
 import Collection from '@/features/my-collection/components/Collection';
 import { useCollections } from '@/features/my-collection/hooks';
+import Loading from '@/shared/ui/loading/Loading';
 import React from 'react';
 
 export default function CollectionList() {
   const { data, isLoading, isFetching } = useCollections();
 
   if (isFetching || isLoading) {
-    return (
-      <div className="w-full p-[1.6rem] justify-center items-center flex flex-col gap-[2rem] h-dvh">
-        {/* TODO: 스피터 아이콘 추가 */}
-        <></>
-        <div className="flex flex-col gap-[0.8rem] justify-center items-center">
-          <p className="text-[1.8rem] font-bold">관심 뉴스를 불러오고 있는 중입니다.</p>
-          <p className="text-[1.4rem] font-normal">잠시만 기다려 주세요.</p>
-        </div>
-      </div>
-    );
+    return <Loading />;
   }
 
   if (data && data.totalCount === 0) {
