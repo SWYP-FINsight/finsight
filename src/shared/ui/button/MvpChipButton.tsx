660f52f48ecb93299d354f714343ce549717b344
sunny-son <thstkd92@gmail.com>
2025-11-16 18:30:05 +0900

    style : MvpChipButton 추가

---
+++ src/shared/ui/button/MvpChipButton.tsx
@@ -0,0 +1,26 @@
+import { cn } from '@/lib/utils';
+
+interface Props {
+  title: string;
+  size?: 'lg' | 'sm';
+  isActive?: boolean;
+  onClick?: () => void;
+}
+
+export default function MvpChipButton({ title, size = 'lg', isActive = false, onClick, ...rest }: Props) {
+  return (
+    <button
+      className={cn(
+        'flex h-[3.2rem] py-[0.6rem] px-[1.2rem] justify-center items-center rounded-[0.8rem] text-12 font-medium cursor-pointer',
+        isActive ? 'bg-gray-700 text-[1.2rem] text-gray-50' : 'border border-solid border-gray-300 bg-gray-50',
+        {
+          'w-[13rem]': size === 'lg',
+          'w-[8.4rem]': size === 'sm',
+        },
+      )}
+      {...rest}
+    >
+      {title}
+    </button>
+  );
+}
