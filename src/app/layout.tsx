a88d67535d7bcd8c7cb48cbfb475db8c80708fdf
mmeat512 <58766047+mmeat512@users.noreply.github.com>
2025-11-25 23:45:06 +0900

    feat: og-image 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,23 @@ export const metadata: Metadata = {
   icons: {
     icon: '/favicon.png',
   },
-  description: '초심자들을 위한 나만의 맞춤형 투자 콘텐츠 Collector.',
+  description:
+    '내가 관심있어 하는 정보, 나의 자산과 직접적인 관련이 있는 금융 정보들만 놓치지 않고 따끈따끈하게 소화하고 싶지 않으신가요? FINsight와 함께라면 간단한 키워드 설정만으로 나에게 꼭 필요한, 최신의 투자 관련 뉴스들을 하나의 플랫폼에서 모두 받아보세요!',
+  openGraph: {
+    title: 'FINsight',
+    description:
+      '내가 관심있어 하는 정보, 나의 자산과 직접적인 관련이 있는 금융 정보들만 놓치지 않고 따끈따끈하게 소화하고 싶지 않으신가요? FINsight와 함께라면 간단한 키워드 설정만으로 나에게 꼭 필요한, 최신의 투자 관련 뉴스들을 하나의 플랫폼에서 모두 받아보세요!',
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'FINsight',
+      },
+    ],
+    url: 'https://finsight-publish.vercel.app/',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
