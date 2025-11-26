ab3a59e084d4f352e9c89a3d18d21d96ed67eb7c
sunny-son <thstkd92@gmail.com>
2025-11-26 17:43:23 +0900

    fix : 카카오 리다이렉트 URL 변경

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -2,4 +2,4 @@
 export const QUERY_STALE_TIME = 1000 * 60 * 5; // 5분 동안 캐시된 데이터 사용
 
 // OAuth 로그인
-export const CLIENT_KAKAO = `${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/kakao`;
+export const CLIENT_KAKAO = `https://finsight-publish.vercel.app/api/oauth2/authorization/kakao`;
